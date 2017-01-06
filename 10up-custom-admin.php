69547a5e522a18023b81a14a6c9ae6d04e7e2dac
Gabriel Luethje <gabriel.luethje@fusion.net>
2017-01-06 11:06:49 -0600

    Initial commit

---
+++ 10up-custom-admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     10up Custom Login Page
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A plugin that customizes the login page for 10up
+ * Author:          Gabriel Luethje
+ * Author URI:      http://github.com/fstop
+ * Text Domain:     10up-custom-admin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         10up_Custom_Admin
+ */
